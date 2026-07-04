5735093ef0862dd25a19e64fa1779c92a782362d
bogdanaoancea <bogdanaoancea01@gmail.com>
2026-07-04 14:55:44 +0300

    Add message field for response

--- lib/controllers/meme_controller.rb
+++ lib/controllers/meme_controller.rb
@@ -4,12 +4,13 @@ require './lib/services/image_download_service'
 require './lib/services/meme_generator_service'
 require './lib/dtos/meme'
 require './lib/dtos/response'
+require "json"
 
 class MemeController
   def execute(body)
-    
+
     if body.nil? || body.empty?
-      return Response.new(400)
+      return Response.new(400, "Empty body")
     end
 
     meme_json = body["meme"]
@@ -20,17 +21,17 @@ class MemeController
     )
 
     if meme_info.image_url.nil? || meme_info.text.nil?
-      return Response.new(400)
+      return Response.new(400, "Check url and text fields")
     end
 
     img_service_response = ImageDownloadService.download(meme_info.image_url)
 
     if img_service_response.nil?
-      Response.new(400)
+      Response.new(400, "No image downloaded")
     else
       meme_service_response = MemeGeneratorService.generate(img_service_response, meme_info.text)
 
-      Response.new(303, File.basename(meme_service_response))
+      Response.new(303, "Success", File.basename(meme_service_response))
     end
   end
 end
--- lib/dtos/response.rb
+++ lib/dtos/response.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Response
-  attr_reader :response_status, :redirect_url
+  attr_reader :response_status, :message, :redirect_url
 
-  def initialize(response_status, redirect_url = nil)
+  def initialize(response_status, message = nil, redirect_url = nil)
     @response_status = response_status
+    @message = message
     @redirect_url = redirect_url
   end
 end
