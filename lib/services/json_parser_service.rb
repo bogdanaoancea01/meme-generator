68220204ec2d9c6bbe5d44a36d1d6c1a21c3cdf9
bogdanaoancea <bogdanaoancea01@gmail.com>
2026-07-06 12:47:17 +0300

    Add JSON parser service

--- lib/services/json_parser_service.rb
+++ lib/services/json_parser_service.rb
@@ -1,10 +1,13 @@
-class JsonParser
+class JsonParserService
     def parse(body)
         if body.nil? || body.empty?
             return nil
         end
         
         root = body["meme"]
+        if root.nil? || root.empty?
+            return nil
+        end
         Meme.new(root["image_url"], root["text"])
     end
 end
