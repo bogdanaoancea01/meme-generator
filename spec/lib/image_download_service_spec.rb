237c2b08c70dde8bb2da73c0290a86973d40f025
bogdanaoancea <bogdanaoancea01@gmail.com>
2026-07-06 13:00:51 +0300

    Mock image downloads in image download service tests

--- spec/lib/image_download_service_spec.rb
+++ spec/lib/image_download_service_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe ImageDownloadService do
   describe '.download' do
     context 'the given url exists / is correct' do
       let(:url) { 'https://picsum.photos/200' }
+      let(:img_file) { File.open("spec/fixtures/test_original_1.png", "rb") }
 
       it 'downloads an image and saves it to images folder, returning the path' do
+        allow(URI).to receive(:open).with(url).and_yield(img_file)
         file_path = described_class.download(url)
 
         expect(File.exist?(file_path)).to be true
@@ -19,6 +21,10 @@ RSpec.describe ImageDownloadService do
       let(:url) { 'https://picsum.photos/200dddddddddd' }
 
       it 'catches an error and returns nil' do
+        allow(URI).to receive(:open)
+          .with(url)
+          .and_raise(StandardError)
+
         response = described_class.download(url)
 
         expect(response).to be(nil)
