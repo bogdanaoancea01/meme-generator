4370ca6c614206ab9755ec962218abb03a7a5e7b
bogdanaoancea <bogdanaoancea01@gmail.com>
2026-07-07 10:24:24 +0300

    Split api spec based on the tested endpoint

--- spec/lib/meme_api_spec.rb
+++ spec/lib/meme_api_spec.rb
@@ -10,7 +10,6 @@ require './lib/dtos/response'
 require_relative '../spec_helper'
 
 RSpec.describe 'Meme API' do
-
   include Rack::Test::Methods
 
   def app
@@ -20,7 +19,7 @@ RSpec.describe 'Meme API' do
   context 'when the request is correct' do
     let(:body) { File.read('spec/fixtures/meme_test.json') }
 
-    it 'it returns status code 303' do
+    it 'returns status code 303' do
       post '/memes', body, { 'CONTENT_TYPE' => 'application/json' }
 
       expect(last_response.status).to eq(303)
@@ -72,53 +71,6 @@ RSpec.describe 'Meme API' do
       expect(last_response.status).to eq(400)
       response_body = JSON.parse(last_response.body)['message']
       expect(response_body).to include('Empty body')
-    end
-  end
-
-  context 'sign up user' do
-    let(:body) { File.read('spec/fixtures/user/user_test.json') }
-
-    it 'returns status code 201 and user token' do
-      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
-
-      expect(last_response.status).to eq(201)
-      response_body = JSON.parse(last_response.body)['token']
-      expect(response_body).to include('aaaa')
-    end
-  end
-
-  context 'the username does not exist in the request' do
-    let(:body) { File.read('spec/fixtures/user/no_username_test.json') }
-
-    it 'returns status code 400 and error message' do
-      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
-
-      expect(last_response.status).to eq(400)
-      response_body = JSON.parse(last_response.body)['errors']
-      expect(response_body).to include({ "message" => "Username is blank" })
-    end
-  end
-
-  context 'the password does not exist in the request' do
-    let(:body) { File.read('spec/fixtures/user/no_password_test.json') }
-
-    it 'returns status code 400 and error message' do
-      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
-
-      expect(last_response.status).to eq(400)
-      response_body = JSON.parse(last_response.body)['errors']
-      expect(response_body).to include({ "message" => "Password is blank" })
-    end
-  end
-
-  context 'when the username already exists' do
-    let(:body) { File.read('spec/fixtures/user/user_test.json') }
-
-    it 'returns 409' do
-      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
-      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
-
-      expect(last_response.status).to eq(409)
     end
   end
 end
---
+++ spec/lib/user/signup_api_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../../../api'
+require 'rspec'
+require 'rack/test'
+require_relative '../../spec_helper'
+
+RSpec.describe 'Signup API' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  context 'sign up user' do
+    let(:body) { File.read('spec/fixtures/user/user_test.json') }
+
+    it 'returns status code 201 and user token' do
+      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response.status).to eq(201)
+      response_body = JSON.parse(last_response.body)['token']
+      expect(response_body).to include('aaaa')
+    end
+  end
+
+  context 'the username does not exist in the request' do
+    let(:body) { File.read('spec/fixtures/user/no_username_test.json') }
+
+    it 'returns status code 400 and error message' do
+      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response.status).to eq(400)
+      response_body = JSON.parse(last_response.body)['errors']
+      expect(response_body).to include({ "message" => "Username is blank" })
+    end
+  end
+
+  context 'the password does not exist in the request' do
+    let(:body) { File.read('spec/fixtures/user/no_password_test.json') }
+
+    it 'returns status code 400 and error message' do
+      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response.status).to eq(400)
+      response_body = JSON.parse(last_response.body)['errors']
+      expect(response_body).to include({ "message" => "Password is blank" })
+    end
+  end
+
+  context 'when the username already exists' do
+    let(:body) { File.read('spec/fixtures/user/user_test.json') }
+
+    it 'returns 409' do
+      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
+      post '/signup', body, { 'CONTENT_TYPE' => 'application/json' }
+
+      expect(last_response.status).to eq(409)
+    end
+  end
+end
